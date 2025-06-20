d7939332eed45f4d3bbda94d5a3b6fd56feb3322
nazar2012 <133564811+nazar2012@users.noreply.github.com>
2025-06-20 12:40:25 +0300

    wyrt

--- hw-11.js
+++ hw-11.js
@@ -2,7 +2,6 @@ const bankAccount = {
     ownerName: "Nazar",
     accountNumber: "12345687",
     balance: 1000,
-
     deposit(amount) {
         this.balance += amount
         alert(`ваш рахунок поповнено на ${amount} загальна сума на вашому рахунку ${this.balance}`)
@@ -36,13 +35,15 @@ const weather = {
     temperature: 0,
     humidity: 50,
     windSpeed: 10,
-    isBelowZero: function () {
-        return this.temperature < 0;
+    isBelowZero() {
+        const { temperature } = this;
+        return temperature < 0;
     }
 };
 const tempInput = prompt("Введіть температуру в градусах Цельсія:");
 weather.temperature = parseFloat(tempInput);
-if (weather.isBelowZero()) {
+const { temperature } = weather;
+if (temperature < 0) {
     console.log("Температура нижче 0 градусів Цельсія");
 } else {
     console.log("Температура вище або рівна 0 градусів Цельсія");
@@ -52,14 +53,16 @@ const user = {
     name: "Назар",
     email: "gromenkonazar@gmail.com",
     password: "123456",
-    login: function (inputEmail, inputPassword) {
-        return this.email === inputEmail && this.password === inputPassword;
+    login(inputEmail, inputPassword) {
+        const { email, password } = this;
+        return email === inputEmail && password === inputPassword;
     }
 };
 const emailInput = prompt("Введіть email:");
 const passwordInput = prompt("Введіть пароль:");
+const { name: userName } = user;
 if (user.login(emailInput, passwordInput)) {
-    console.log(`Вхід успішний. Ласкаво просимо, ${user.name}!`);
+    console.log(`Вхід успішний. Ласкаво просимо, ${userName}!`);
 } else {
     console.log("Невірний email або пароль");
 }
@@ -69,14 +72,17 @@ const movie = {
     director: "Christopher Nolan",
     year: 2010,
     rating: 7,
-    isHighlyRated: function () {
-        return this.rating > 8;
+    isHighlyRated() {
+        const { rating } = this;
+        return rating > 8;
     }
 };
-console.log("Назва:", movie.title);
-console.log("Режисер:", movie.director);
-console.log("Рік:", movie.year);
-console.log("Рейтинг:", movie.rating);
+const { title, director, year, rating } = movie;
+console.log("Назва:", title);
+console.log("Режисер:", director);
+console.log("Рік:", year);
+console.log("Рейтинг:", rating);
+const { isHighlyRated } = movie;
 if (movie.isHighlyRated()) {
     console.log("Фільм має високий рейтинг");
 } else {
